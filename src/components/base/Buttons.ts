4a04c939d76d6370989871ee46cd40d8787a2dc3
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-10-15 17:13:58 +0200

    Add storybook and prettier and eslint config files

---
+++ src/components/base/Buttons.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  border: 1px solid blue;
+  background-color: white;
+  color: blue;
+  border-radius: 4px;
+`
+
+export default Button
