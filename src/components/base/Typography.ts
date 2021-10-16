677a989cbd8a2ddf9c878f7c31cc42d880e67cae
bercivarga <65171545+bercivarga@users.noreply.github.com>
2021-10-16 22:36:29 +0200

    Add typography base components to project

---
+++ src/components/base/Typography.ts
@@ -0,0 +1,50 @@
+import styled, {ThemeProps} from "styled-components";
+import {typeScale, ThemeI} from "../utils";
+
+export const Paragraph = styled.p`
+  font-size: ${typeScale.paragraph};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Header1 = styled.h1`
+  font-size: ${typeScale.header1};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Header2 = styled.h2`
+  font-size: ${typeScale.header2};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Header3 = styled.h3`
+  font-size: ${typeScale.header3};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Header4 = styled.h4`
+  font-size: ${typeScale.header4};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Header5 = styled.h5`
+  font-size: ${typeScale.header5};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor}
+`
+
+export const Span = styled.span`
+  font-size: ${typeScale.paragraph};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor};
+`
+
+export const HelperText = styled.span`
+  font-size: ${typeScale.helperText};
+  font-family: ${(props: ThemeProps<ThemeI>) => props.theme.primaryFont};
+  color: ${(props: ThemeProps<ThemeI>) => props.theme.textColor};
+`
